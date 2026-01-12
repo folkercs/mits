cec8c76405b7770c9bea8ebce5958a2f55b230b8
Folker Csaba <folkercs@gszi.edu.hu>
2026-01-12 09:25:38 +0100

    0112  -RestaurantController

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Services\RestaurantService;
 
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
-    //
+    public function list($id)
+    {
+
+    }
+
+    public function listall()
+    {
+
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestaurantController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,5 +11,6 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function () {
     Route::get('/status',fn()=>response()->json(['status' =>'ok']));
-
+    Route::get('/restaurant/{id}',[RestaurantController::class,'list']);
+    Route::get('/restaurants',[RestaurantController::class,'listall']);    
 });
